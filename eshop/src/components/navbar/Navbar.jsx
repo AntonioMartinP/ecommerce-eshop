aece41586b748f915ae09838fb1b12b20c845ab2
AntonioMartinP <josoner371@exitbit.com>
2025-08-25 07:18:31 +0200

    LOGO

---
+++ eshop/src/components/navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="bg-white shadow-md dark:bg-gray-900 dark:text-white duration-200 relative z-40">
+        <div className="py-4">
+            <div className="container">
+                
+                {/*Logo and links section*/}
+                <div>
+                    <a href="#" className="text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl">Eshop</a>
+                {/*Menu items */}
+                <div>
+
+                </div>
+                </div>
+                {/*Navbar right section*/}
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
